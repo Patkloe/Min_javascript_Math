d166ce1f38933b7c81b5c3288fc609cc44bddd83
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-09-09 09:53:29 -0500

    Create minnbre.js

---
+++ minnbre.js
@@ -0,0 +1,22 @@
+// 1 case
+
+function min(x,y){
+ return Math.min(x,y);
+}
+  min(0,-10);
+
+// 2 case
+
+function minnbre(x,y){
+ var a = Number(x);
+ var b = Number(y);
+ if(a > b)
+   console.log(b);
+ else if (b > a)
+   console.log(a);
+ else
+   console.log(a);
+}
+minnbre(0,-10);
+minnbre(0,10);
+minnbre(10,10);
